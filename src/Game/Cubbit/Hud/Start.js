ae7a716afb014cf989285ac9a3ab7c4c1c2bd4d9
aratama <aratama@example.com>
2016-12-29 10:07:17 +0900

    tweak

--- src/Game/Cubbit/Hud/Start.js
+++ src/Game/Cubbit/Hud/Start.js
@@ -1,8 +1,10 @@
 exports.reportToSentry = function(){
-    setTimeout(function(){
-        Raven.captureException(new Error('Cubbit may be going well.'), {
-            level: 'info'
-        });
-    }, 10000);
+    if(window.location.host === "aratama.github.io"){
+        setTimeout(function(){
+            Raven.captureException(new Error('Cubbit may be going well.'), {
+                level: 'info'
+            });
+        }, 10000);
+    }
     exports.reportToSentry = function(){};
 };
